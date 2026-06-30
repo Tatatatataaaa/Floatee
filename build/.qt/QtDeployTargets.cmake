26065e9d6b426f3d5638695afb0bb5257d69ddc3
Tata <2300013408@stu.pku.edu.cn>
2026-06-30 23:24:24 +0800

    Add .gitignore, exclude build directories

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Floatee_FILE /Users/tata/Library/CloudStorage/OneDrive-个人/Floatee/Floatee_250912/Floatee/build/Floatee.app/Contents/MacOS/Floatee)
-set(__QT_DEPLOY_TARGET_Floatee_TYPE EXECUTABLE)
